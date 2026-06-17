41f8c69b3510b03eda46bfdab7515bfa17c36906
Valentín Sterba <valentin.sterba@gmail.com>
2026-06-17 12:28:50 -0300

    Comentarios

--- Recuperatorio/parcial-roto/index.js
+++ Recuperatorio/parcial-roto/index.js
@@ -1,80 +1,80 @@
 //--- Funcion que obtiene el carrito del LocalStorage, lo parsea a un array y lo retorna ---//
 function obtenerCarrito() {
-    let carrito = localStorage.getItem('carrito');
-    return carrito ? JSON.parse(carrito) : [];
+    let carrito = localStorage.getItem('carrito'); //Buscamos el carrito en el localStorage.
+    return carrito ? JSON.parse(carrito) : []; //Si el carrito NO está vacío, lo retornamos parseado a un array. Si está vacío, retornamos un array vacío.
 }
 
 //--- Funcion que guarda el carrito recibido al LocalStorage, previamente transformado a string ---//
 function guardarCarrito(carrito) {
-    let carritoString = JSON.stringify(carrito);
-    localStorage.setItem('carrito', carritoString);
-    actualizarContadorCarrito();
+    let carritoString = JSON.stringify(carrito); //Parseamos el carrito recibido como parámetro a un objeto JSON.
+    localStorage.setItem('carrito', carritoString); //Guardamos el carrito parseado en el localStorage con la clave 'carrito'.
+    actualizarContadorCarrito(); //Actualizamos el contador de productos.
 }
 
 //--- Funcion que actualiza el contador de productos en el carrito ---//
 function actualizarContadorCarrito() {
-    let contadorCarrito = document.getElementById('cantidad-carrito');
-    let cantidadProductos = 0;
+    let contadorCarrito = document.getElementById('cantidad-carrito'); //Seleccionamos el elemento que contiene la cantidad de productos.
+    let cantidadProductos = 0; //Inicializamos la cantidad de productos en 0.
 
-    let carrito = obtenerCarrito();
+    let carrito = obtenerCarrito(); //Obtenemos el carrito.
 
-    if (carrito) {
-        carrito.forEach(producto => cantidadProductos += producto.cantidad);
+    if (carrito) { //Si el carrito NO es nulo...
+        carrito.forEach(producto => cantidadProductos += producto.cantidad); //Por cada producto, acumular su cantidad en cantidadProductos.
     }
 
-    contadorCarrito.textContent = cantidadProductos;
+    contadorCarrito.textContent = cantidadProductos; //Cambiar el contenido de texto del elemento para que muestre la cantidad de productos.
 }
 
 function sumarAlCarrito(e) {
     //--- Obtengo la referencia al elemento clickeado desde en base al evento (Propiedad exclusivamente de todos los Events) ---//
     let elementoClickeado = e.target;
 
-    let elementoProducto = elementoClickeado.closest('li');
-    let nombreProducto = elementoProducto.querySelector('.nombre-producto').textContent;
-    let textoPrecioProducto = elementoProducto.querySelector('.precio-producto').textContent;
-    let precioProducto = parseInt(textoPrecioProducto.replace('$', ''));
+    let elementoProducto = elementoClickeado.closest('li'); //Obtenemos el ancestro de tipo 'li' más cercano al botón.
+    let nombreProducto = elementoProducto.querySelector('.nombre-producto').textContent; //Buscamos el nombre del producto.
+    let textoPrecioProducto = elementoProducto.querySelector('.precio-producto').textContent; //Buscamos el texto del precio del producto.
+    let precioProducto = parseInt(textoPrecioProducto.replace('$', '')); //Al texto del producto le sacamos el signo '$' y parseamos el resto a entero.
 
-    let carrito = obtenerCarrito();
+    let carrito = obtenerCarrito(); //Obtenemos el carrito.
 
-    let productoExistente = carrito.find(producto => producto.nombre === nombreProducto);
+    let productoExistente = carrito.find(producto => producto.nombre === nombreProducto); //Buscamos el producto en el carrito mediante su nombre.
 
-    if (productoExistente) {
-        productoExistente.cantidad += 1;
+    if (productoExistente) { //Si el producto está en el carrito...
+        productoExistente.cantidad += 1; //Aumentamos su cantidad en 1.
     }
-    else {
-        carrito.push({ 'nombre': nombreProducto, 'cantidad': 1, 'precioUnitario': precioProducto });
+    else { //Si NO está en el carrito...
+        carrito.push({ 'nombre': nombreProducto, 'cantidad': 1, 'precioUnitario': precioProducto }); //Lo agregamos directamente con la cantidad inicializada en 1.
     }
 
-    guardarCarrito(carrito);
-    alert(`Un/Una ${nombreProducto} se agregó al carrito.`);
-    actualizarContadorCarrito();
+    guardarCarrito(carrito); //Guardamos el carrito actualizado.
+    alert(`Un/Una ${nombreProducto} se agregó al carrito.`); //Lanzamos una alerta para confirmar que el producto se agregó exitosamente.
+    actualizarContadorCarrito(); //Actualizamos el contador de productos.
 }
 
 function restarDelCarrito(e) {
     //--- Obtengo la referencia al elemento clickeado desde en base al evento (Propiedad exclusivamente de todos los Events) ---//
     let elementoClickeado = e.target;
 
-    let elementoProducto = elementoClickeado.closest('li');
-    let nombreProducto = elementoProducto.querySelector('.nombre-producto').textContent;
+    let elementoProducto = elementoClickeado.closest('li'); //Obtenemos el ancestro de tipo 'li' más cercano al botón.
+    let nombreProducto = elementoProducto.querySelector('.nombre-producto').textContent; //Buscamos el nombre del producto.
 
-    let carrito = obtenerCarrito();
+    let carrito = obtenerCarrito(); //Obtenemos el carrito.
 
-    let productoExistente = carrito.find(producto => producto.nombre === nombreProducto);
+    let productoExistente = carrito.find(producto => producto.nombre === nombreProducto); //Buscamos el producto en el carrito mediante su nombre.
 
-    if (productoExistente) {
-        productoExistente.cantidad -= 1;
-        let mensaje = `Un/Una ${nombreProducto} se restó del carrito.`;
-        if (productoExistente.cantidad <= 0) {
-            carrito = carrito.filter(producto => producto.nombre !== nombreProducto);
-            mensaje = `Un/Una ${nombreProducto} se eliminó del carrito.`;
+    if (productoExistente) { //Si el producto está en el carrito...
+        productoExistente.cantidad -= 1; //Reducimos su cantidad en 1.
+        let mensaje = `Un/Una ${nombreProducto} se restó del carrito.`; //Le asignamos el valor por defecto al mensaje.
+        if (productoExistente.cantidad <= 0) { //Si la cantidad final es menor o igual a 0...
+            carrito = carrito.filter(producto => producto.nombre !== nombreProducto); //Usamos un filtro para dejar afuera el producto.
+            mensaje = `Un/Una ${nombreProducto} se eliminó del carrito.`; //Cambiamos el valor del mensaje.
         }
-        guardarCarrito(carrito);
-        alert(mensaje);
+        guardarCarrito(carrito); //Guardamos el carrito actualizado.
+        alert(mensaje); //Lanzamos una alerta con el mensaje correcto.
     }
-    else {
-        alert(`No hay más ${nombreProducto} en el carrito.`);
+    else { //Si el producto NO está en el carrito...
+        alert(`No hay más ${nombreProducto} en el carrito.`); //Lanzamos una alerta que nos lo indica.
     }
-    actualizarContadorCarrito();
+    actualizarContadorCarrito(); //Actualizamos el contador de productos.
 }
 
 //--- Funcion para ordenar productos de mayor a menor precio ---//
@@ -119,16 +119,16 @@ function ordenarMenorAMayor(e) {
 
 //--- Funcion para mostrar/ocultar las calorías de las hamburguesas ---//
 function toggleCalorias(e) {
-    let botonCalorias = e.target;
+    let botonCalorias = e.target; //Asociamos el botón clickeado al evento.
 
-    let elementoProducto = botonCalorias.closest('li');
-    let contenedorCalorias = elementoProducto.querySelector('.calorias-producto');
+    let elementoProducto = botonCalorias.closest('li'); //Buscamos el ancestro de tipo 'li' más cercano al botón.
+    let contenedorCalorias = elementoProducto.querySelector('.calorias-producto'); //Desde el ancestro 'li', buscamos el elemento que contiene el texto con las calorías.
 
-    if (contenedorCalorias.style.display === 'none') {
-        contenedorCalorias.style.display = 'block';
+    if (contenedorCalorias.style.display === 'none') { //Si su atributo 'display' tiene el valor 'none' (es decir, está oculto)...
+        contenedorCalorias.style.display = 'block'; //Cambiamos el valor a 'block' para que se muestre.
     }
-    else {
-        contenedorCalorias.style.display = 'none';
+    else { //SI el valor es 'block' (es decir, NO está oculto)...
+        contenedorCalorias.style.display = 'none'; //Cambiamos el valor a 'none' para ocultarlo.
     }
 }
 
--- Recuperatorio/parcial-roto/pages/carrito/carrito.js
+++ Recuperatorio/parcial-roto/pages/carrito/carrito.js
@@ -1,39 +1,40 @@
 function obtenerCarrito() {
-    let carrito = localStorage.getItem('carrito');
-    let carritoArray = JSON.parse(carrito);
-    return carritoArray;
+    let carrito = localStorage.getItem('carrito'); //Buscamos el carrito en el localStorage.
+    let carritoArray = JSON.parse(carrito); //Lo parseamos a un array.
+    return carritoArray; //Lo retornamos.
 }
 
 function cargarProductosCarrito() {
-    let headingPrecioFinal = document.getElementById('valor-final');
-    let tabla = document.getElementById("tabla-carrito");
-    let cabeceras = tabla.querySelector('.fila-header-carrito');
-    let precioFinal = 0;
+    let headingPrecioFinal = document.getElementById('valor-final'); //Asociamos el heading que contiene el texto con el precio final.
+    let tabla = document.getElementById("tabla-carrito"); //Asociamos la tabla con los productos del carrito.
+    let precioFinal = 0; //Inicializamos el precio final.
+    //Creamos las cabeceras de la tabla.
     tabla.innerHTML = `<tr class="fila-header-carrito">
         <td class="celda-header-tabla-carrito">Nombre del producto</td>
         <td class="celda-header-tabla-carrito">Cantidad</td>
         <td class="celda-header-tabla-carrito">Precio unitario</td>
     </tr>`
 
-    let carrito = obtenerCarrito();
+    let carrito = obtenerCarrito(); //Obtenemos el carrito.
 
-    if (carrito) {
-        carrito.forEach(producto => {
+    if (carrito) { //Si el carrito NO está vacío...
+        carrito.forEach(producto => { //Por cada producto...
+            //Creamos un 'tr' con los datos del producto (nombre, cantidad y precio unitario) y se lo agregamos a la tabla.
             tabla.innerHTML += `<tr><td>${producto.nombre}</td><td>${producto.cantidad}</td><td>$${producto.precioUnitario}</td></tr>`;
-            precioFinal += producto.cantidad * producto.precioUnitario;
+            precioFinal += producto.cantidad * producto.precioUnitario; //Acumulamos en 'precioFinal' el producto entre la cantidad y el precio unitario.
         });
     }
-    else {
-        tabla.innerHTML += '<td colspan="3">No hay productos en el carrito.</td>';
+    else { //Si el carrito está vacío...
+        tabla.innerHTML += '<td colspan="3">No hay productos en el carrito.</td>'; //Mostramos un texto en la tabla que lo indique.
     }
 
-    headingPrecioFinal.textContent = `El valor final a pagar es de: $${precioFinal}`;
+    headingPrecioFinal.textContent = `El valor final a pagar es de: $${precioFinal}`; //Actualizamos el texto del heading que contiene el precio final.
 }
 
 function limpiarCarrito() {
-    localStorage.removeItem('carrito');
-    cargarProductosCarrito();
-    alert('Carrito limpiado exitosamente.');
+    localStorage.removeItem('carrito'); //Borramos los datos del carrito.
+    cargarProductosCarrito(); //Actualizamos la tabla de productos del carrito.
+    alert('Carrito limpiado exitosamente.'); //Lanzamos una alerta para confirmar.
 }
 
 // Asociar evento al botón cuando la página carga
